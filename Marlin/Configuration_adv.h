5db4f141567bfce4d1303ad736a03e2380252515
Luiz Eduardo <servergta@gmail.com>
2020-10-25 22:13:23 -0300

    Change cancel script

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1146,7 +1146,7 @@
 
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
-  #define EVENT_GCODE_SD_ABORT "G91\nG0 E-10 F1200\nG0 Z20 F900\nG90\nG0 X" STRINGIFY(X_MAX_POS) " F9000\nM18 X Y Z E0\nM107"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_ABORT "G91\nG0 E-10 F1500\nG90\nM18 X Y Z E0\nM107\nM118 A1 action:cancel"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
 
   #if ENABLED(PRINTER_EVENT_LEDS)
     #define PE_LEDS_COMPLETED_TIME  (30*60) // (seconds) Time to keep the LED "done" color before restoring normal illumination
